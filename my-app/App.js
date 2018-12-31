a11abb4855f1eb03165acc522c5dc90ddc74c9c1
Jack <jitesh.webdesksolution@gmail.com>
2018-12-31 17:49:18 +0530

    Please stat again

--- my-app/App.js
+++ my-app/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import AppNavigator from './navigation/AppNavigation'
 
 export default class App extends React.Component {
   render() {
     return (
-      <AppNavigator/>
     );
   }
 }
