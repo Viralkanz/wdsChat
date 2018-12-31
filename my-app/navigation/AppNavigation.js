243b3a504fe24fbc839deca6d8d03657ae2c74cf
Vivek <vivek.webdesk@gmail.com>
2018-12-31 16:57:58 +0530

    31-12-2018 commit for assets

--- my-app/navigation/AppNavigation.js
+++
@@ -1,9 +0,0 @@
-import {createStackNavigator} from 'react-navigation';
-import otp from '../screens/otp';
-
-const App = createStackNavigator({
-  otp: {screen: otp},
-  
-});
-
-export default App;
